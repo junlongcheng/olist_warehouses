b46f79625309d32aa71ad5528a67b5af807bda52
junlongcheng <junlong.cheng@student.ie.edu>
2024-06-20 17:52:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ else:
     )
 
     # Display Plot
-    st.plotly_chart(fig, use_container_width=True)  # Expand plot to full width
+    st.plotly_chart(fig, use_container_width=True, width=1000, height=1000)
